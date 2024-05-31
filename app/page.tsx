ff58f11c8e5c97b3529f5c2ca0feff475a267251
pedrocruzio <pedro@rokailabs.com>
2024-05-31 11:55:08 -0500

    redirect home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,6 @@
-"use client";
+import { redirect } from 'next/navigation';
 
-import React from 'react';
-import MonacoEditor from '../components/MonacoEditor';
-import Instructions from '../components/Instructions';
-import Console from '../components/Console';
-import Navigation from '../components/Navigation';
-
-const Home: React.FC = () => {
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="flex flex-1 overflow-hidden">
-        <div className="w-1/3 h-full">
-          <Instructions />
-        </div>
-        <div className="w-2/3 flex flex-col h-full">
-          <div className="flex-1 h-full">
-            <MonacoEditor />
-          </div>
-          <Console />
-        </div>
-      </div>
-      <Navigation />
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  redirect('/lesson/1');
+  return null;
+}
